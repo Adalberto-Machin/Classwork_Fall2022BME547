fd9f9851aa535fb4ba71c0163b55203b0f5f8f83
m4c11in <70716647+m4c11in@users.noreply.github.com>
2022-10-24 11:24:54 -0400

    Made both files pep-8 compliant

--- Time_Server.py
+++ Time_Server.py
@@ -5,27 +5,28 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route("/time", methods =["GET"])
+@app.route("/time", methods=["GET"])
 def server_time():
-    # return the current time as a string 
+    # return the current time as a string
     time = datetime.now().time()
     time_string = str(time)
     return jsonify(time_string)
 
 
-@app.route("/date", methods =["GET"])
+@app.route("/date", methods=["GET"])
 def server_date():
     # current date as a string
     date = datetime.now().date()
     date_string = datetime.strftime(date, "%m-%d-%y %H:%M:%S")
     return jsonify(date_string)
 
+
 def get_date():
     date = datetime.now().date()
     return date
 
 
-@app.route("/age", methods =["POST"])
+@app.route("/age", methods=["POST"])
 def age_substract():
     """
         incoming_json = {"date": <10/10/1999>,
@@ -37,36 +38,35 @@ def age_substract():
     date_object_age_origin = datetime.strptime(age_origin['date'], "%m/%d/%Y")
     my_time = datetime.min.time()
     age = datetime.combine(date, my_time) - date_object_age_origin
-    return jsonify(round(age.days/365,2))
+    return jsonify(round(age.days/365, 2))
 
 
-@app.route("/until_next_meal/<meal>", methods =["GET"])
+@app.route("/until_next_meal/<meal>", methods=["GET"])
 def server_meal_time(meal):
     time_now = datetime.now().time()
     date = get_date()
     origin_time_now = datetime.combine(date, time_now)
     if meal == "breakfast":
         # gets time till breakfast
-        origin_breakfast = datetime.combine(date,time(9, 0, 0))
+        origin_breakfast = datetime.combine(date, time(9, 0, 0))
         breakfast_time_diff = origin_breakfast - origin_time_now
         answer = breakfast_time_diff.seconds/(60*60)
-        return jsonify(round(answer,2))
+        return jsonify(round(answer, 2))
     elif meal == "lunch":
         # gets time till lunch
-        origin_lunch = datetime.combine(date,time(12, 0, 0))
+        origin_lunch = datetime.combine(date, time(12, 0, 0))
         lunch_time_diff = origin_lunch - origin_time_now
         answer = lunch_time_diff.seconds/(60*60)
-        return jsonify(round(answer,2))
+        return jsonify(round(answer, 2))
     elif meal == "dinner":
         # gets time till dinner
-        origin_dinner = datetime.combine(date,time(19, 0, 0))
+        origin_dinner = datetime.combine(date, time(19, 0, 0))
         dinner_time_diff = origin_dinner - origin_time_now
         answer = dinner_time_diff.seconds/(60*60)
-        return jsonify(round(answer,2))
+        return jsonify(round(answer, 2))
     else:
         answer = 'no good input'
-        return jsonify(answer,2)
-
+        return jsonify(answer, 2)
 
 
 if __name__ == "__main__":
--- Time_client.py
+++ Time_client.py
@@ -5,27 +5,22 @@ time = requests.get("http://127.0.0.1:5000/time")
 print(time.status_code)
 print("Time:")
 print(time.text)
-
 date = requests.get("http://127.0.0.1:5000/date")
 print(date.status_code)
 print("Date:")
 print(date.text)
-
 input_age = {'date': "11/20/1998", 'units': "years"}
-age_diff = requests.post("http://127.0.0.1:5000/age", json = input_age)
+age_diff = requests.post("http://127.0.0.1:5000/age", json=input_age)
 print(age_diff.status_code)
 print(f" Age in years: {age_diff.text}")
-
-breakfast_hours = requests.get("http://127.0.0.1:5000/until_next_meal/breakfast")
-print(breakfast_hours.status_code)
+breakfast_h = requests.get("http://127.0.0.1:5000/until_next_meal/breakfast")
+print(breakfast_h.status_code)
 print("Hours till breakfast:")
-print(breakfast_hours.text)
-
+print(breakfast_h.text)
 lunch_hours = requests.get("http://127.0.0.1:5000/until_next_meal/lunch")
 print(lunch_hours.status_code)
 print("Hours till lunch:")
 print(lunch_hours.text)
-
 dinner_hours = requests.get("http://127.0.0.1:5000/until_next_meal/dinner")
 print(dinner_hours.status_code)
 print("Hours till dinner:")
