bd3c145d47e4cdf2c9f2d61c10768408582df013
m4c11in <70716647+m4c11in@users.noreply.github.com>
2022-11-07 13:17:54 -0500

    Created gui interface

---
+++ db_gui.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+
+def main_window():
+    root = tk.Tk()
+    root.title("Blood Donor Database Window")
+    root.geometry("600x300")
+
+    ttk.Label(root, text = "Blood Donor Database").grid(column=0, row=0, columnspan = 2)
+    ttk.Label(root, text="Name:").grid(column=0, row=1)
+    ttk.Entry(root).grid(column=1, row=3)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main_window()
